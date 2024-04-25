770de77eebf1352ae4d9349d0d016c4cfe68762e
David Dossett <ddossett@microsoft.com>
2024-04-24 22:22:32 -0700

    Formatting

--- assistant-with-fc.js
+++ assistant-with-fc.js
@@ -34,43 +34,32 @@ async function main() {
         thread.id,
         {
             role: "user",
-            content: "I want to buy a porsche 911. How much does it cost?"
+            content: "I want to buy a porsche Cayman"
         }
     );
 
     const run = openai.beta.threads.runs.stream(thread.id, {
         assistant_id: assistant.id
-      })
+    })
         .on('textCreated', (text) => process.stdout.write('\nassistant > '))
         .on('textDelta', (textDelta, snapshot) => process.stdout.write(textDelta.value))
         .on('toolCallCreated', (toolCall) => process.stdout.write(`\nassistant > ${toolCall.type}\n\n`))
         .on('toolCallDelta', (toolCallDelta, snapshot) => {
 
-          if (toolCallDelta.type === 'code_interpreter') {
-            if (toolCallDelta.code_interpreter.input) {
-              process.stdout.write(toolCallDelta.code_interpreter.input);
-            }
-            if (toolCallDelta.code_interpreter.outputs) {
-              process.stdout.write("\noutput >\n");
-              toolCallDelta.code_interpreter.outputs.forEach(output => {
-                if (output.type === "logs") {
-                  process.stdout.write(`\n${output.logs}\n`);
+            if (toolCallDelta.type === 'code_interpreter') {
+                if (toolCallDelta.code_interpreter.input) {
+                    process.stdout.write(toolCallDelta.code_interpreter.input);
                 }
-              });
+                if (toolCallDelta.code_interpreter.outputs) {
+                    process.stdout.write("\noutput >\n");
+                    toolCallDelta.code_interpreter.outputs.forEach(output => {
+                        if (output.type === "logs") {
+                            process.stdout.write(`\n${output.logs}\n`);
+                        }
+                    });
+                }
             }
-          }
         });
-
-    if (run.status === 'completed') {
-        const messages = await openai.beta.threads.messages.list(
-            run.thread_id
-        );
-        for (const message of messages.data.reverse()) {
-            console.log(`${message.role} > ${message.content[0].text.value}`);
-        }
-    } else {
-        console.log(run.status);
-    }
 }
 
 main();
